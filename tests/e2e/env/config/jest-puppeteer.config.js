e4b4500eb0c1ebce3728c2520f43bfe178ee320b
Ron Rennick <ron@ronandandrea.com>
2020-05-04 14:37:25 -0300

    combine puppeteer config files

--- tests/e2e/env/config/jest-puppeteer.config.js
+++ tests/e2e/env/config/jest-puppeteer.config.js
@@ -1,8 +1,30 @@
 /** @format */
 
+if ( 'no' == global.process.env.npm_config_dev ) {
+
 module.exports = {
 	launch: {
 		// Required for the logged out and logged in tests so they don't share app state/token.
 		browserContext: 'incognito',
 	},
 };
+
+} else {
+
+module.exports = {
+	launch: {
+		slowMo: process.env.PUPPETEER_SLOWMO ? false : 50,
+		headless: process.env.PUPPETEER_HEADLESS || false,
+		ignoreHTTPSErrors: true,
+		args: [ '--window-size=1920,1080', '--user-agent=chrome' ],
+		devtools: true,
+		defaultViewport: {
+			width: 1280,
+			height: 800,
+		},
+		// Required for the logged out and logged in tests so they don't share app state/token.
+		browserContext: 'incognito',
+	},
+};
+
+}
